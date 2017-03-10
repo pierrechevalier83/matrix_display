7522341198d0c385407d8f4ac62ff23c2fe02351
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-10 23:49:39 +0000

    Write documentation for the cell module

--- src/cell.rs
+++ src/cell.rs
@@ -20,7 +20,16 @@ mod cell_tests {
 }
 
 mod cell {
+    //! Stores data to be printed by MatrixDisplay plus colour metada
+	//!
+	//! - The data type itself is generic. Anything that implements the ToString trait can do
+	//! - The foreground and background color are individually configurable for each cell
 
+	/// A foreground and background color's ansi code
+	///
+	/// # Example:
+	/// `AnsiColor{ fg: 7, bg: 0 }`
+	/// represents a white on dark color
     #[derive(Clone, Debug, PartialEq)]
     pub struct AnsiColor {
         pub fg: u8,
@@ -33,7 +42,33 @@ mod cell {
         }
     }
 
-
+    /// A Matrix Cell that owns some data, a background color and a foreground color
+	///
+	/// The colors are stored by their ansi codes in an AnsiColor struct
+	/// The data can be any type that is clonable and converts to a string
+	///
+	/// # Example (a chess game's representation):
+	/// ```
+	/// let board = vec!['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
+	///                  '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
+	/// 				 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+    ///                  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+	/// 				 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+	/// 				 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+	/// 				 '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖',
+	/// 				 '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙']
+    ///     .iter()
+    ///     .enumerate()
+	/// 	.map(|(i, x)| {
+    ///        let ansi_fg = 33; // pawns in blue
+	/// 		let mut ansi_bg = 0;
+	/// 	    if i % 2 + (i / 8) % 2 == 1 {
+	/// 		    ansi_bg = 7;
+	/// 		}
+	/// 	    cell::Cell::new(x.clone(), ansi_fg, ansi_bg)
+	/// 		})
+    ///     .collect::<Vec<_>>();
+	/// ```
     #[derive(Clone, Debug, PartialEq)]
     pub struct Cell<T>
         where T: Clone,
@@ -46,6 +81,7 @@ mod cell {
         where T: Clone,
               T: ToString
     {
+	    /// Construct a cell with any data, and two ansi codes for the foreground and the background colors
         pub fn new(val: T, fg: u8, bg: u8) -> Cell<T> {
             Cell {
                 value: val,
