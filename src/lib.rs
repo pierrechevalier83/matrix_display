pierrechevalier83/matrix_display

commit 3cc23e676dae843c1073df941b29270ec19097b9

Optimisation: write minimal ANSI sequences
Leverage ansi_term::ANSIStrings to reduce the number of escape
sequences that are printed.

src/lib.rs
@@ -114,24 +114,28 @@ mod matrix_display_tests {
     #[test]
     fn width() {
         let f = Format::new(5, 7);
-        let mut m = Matrix::new(3,
-                            (0..24)
-                                .map(|_| {
-                                    Cell::new(' ', AnsiColor::default().fg, AnsiColor::default().bg)
-                                })
-                                .collect::<Vec<_>>());
+        let mut m = Matrix::new(
+            3,
+            (0..24)
+                .map(|_| {
+                    Cell::new(' ', AnsiColor::default().fg, AnsiColor::default().bg)
+                })
+                .collect::<Vec<_>>(),
+        );
         let d = MatrixDisplay::new(&f, &mut m);
         assert_eq!(3 * 5, d.width());
     }
     #[test]
     fn height() {
         let f = Format::new(5, 7);
-        let mut m = Matrix::new(3,
-                            (0..24)
-                                .map(|_| {
-                                    Cell::new(' ', AnsiColor::default().fg, AnsiColor::default().bg)
-                                })
-                                .collect::<Vec<_>>());
+        let mut m = Matrix::new(
+            3,
+            (0..24)
+                .map(|_| {
+                    Cell::new(' ', AnsiColor::default().fg, AnsiColor::default().bg)
+                })
+                .collect::<Vec<_>>(),
+        );
         let d = MatrixDisplay::new(&f, &mut m);
         assert_eq!(8 * 7, d.height());
     }
@@ -145,69 +149,88 @@ impl<'a> CellDisplay<'a> {
     fn with_borders(b: &'a BordersStyle) -> CellDisplay {
         CellDisplay { borders: b }
     }
-    fn top_cell(&self, pos: &Position, cell_width: usize) -> String {
+    fn top_cell(&self, pos: &Position, cell_width: usize) -> Vec<ansi_term::ANSIString<'static>> {
         if pos.top() {
-            self.cell(self.borders.top_left_corner(),
-                      self.borders.top_intersection(),
-                      self.borders.top_right_corner(),
-                      self.borders.horizontal_border(),
-                      "",
-                      pos,
-                      cell_width,
-                      &AnsiColor::default())
+            self.cell(
+                self.borders.top_left_corner(),
+                self.borders.top_intersection(),
+                self.borders.top_right_corner(),
+                self.borders.horizontal_border(),
+                "",
+                pos,
+                cell_width,
+                &AnsiColor::default(),
+            )
         } else {
-            self.cell(self.borders.left_intersection(),
-                      self.borders.intersection(),
-                      self.borders.right_intersection(),
-                      self.borders.horizontal_border(),
-                      "",
-                      pos,
-                      cell_width,
-                      &AnsiColor::default())
+            self.cell(
+                self.borders.left_intersection(),
+                self.borders.intersection(),
+                self.borders.right_intersection(),
+                self.borders.horizontal_border(),
+                "",
+                pos,
+                cell_width,
+                &AnsiColor::default(),
+            )
         }
     }
-    fn bottom_cell(&self, pos: &Position, cell_width: usize) -> String {
+    fn bottom_cell(
+        &self,
+        pos: &Position,
+        cell_width: usize,
+    ) -> Vec<ansi_term::ANSIString<'static>> {
         if pos.bottom() {
-            self.cell(self.borders.bottom_left_corner(),
-                      self.borders.bottom_intersection(),
-                      self.borders.bottom_right_corner(),
-                      self.borders.horizontal_border(),
-                      "",
-                      pos,
-                      cell_width,
-                      &AnsiColor::default())
+            self.cell(
+                self.borders.bottom_left_corner(),
+                self.borders.bottom_intersection(),
+                self.borders.bottom_right_corner(),
+                self.borders.horizontal_border(),
+                "",
+                pos,
+                cell_width,
+                &AnsiColor::default(),
+            )
         } else {
-            String::new()
+            Vec::new()
         }
     }
-    fn padding_cell(&self, pos: &Position, cell_width: usize, color: &AnsiColor) -> String {
+    fn padding_cell(
+        &self,
+        pos: &Position,
+        cell_width: usize,
+        color: &AnsiColor,
+    ) -> Vec<ansi_term::ANSIString<'static>> {
         self.value_cell(pos, cell_width, " ", color)
     }
-    fn value_cell(&self,
-                  pos: &Position,
-                  cell_width: usize,
-                  content: &str,
-                  color: &AnsiColor)
-                  -> String {
-        self.cell(self.borders.vertical_border(),
-                  self.borders.vertical_border(),
-                  self.borders.vertical_border(),
-                  ' ',
-                  content,
-                  pos,
-                  cell_width,
-                  color)
+    fn value_cell(
+        &self,
+        pos: &Position,
+        cell_width: usize,
+        content: &str,
+        color: &AnsiColor,
+    ) -> Vec<ansi_term::ANSIString<'static>> {
+        self.cell(
+            self.borders.vertical_border(),
+            self.borders.vertical_border(),
+            self.borders.vertical_border(),
+            ' ',
+            content,
+            pos,
+            cell_width,
+            color,
+        )
     }
-    fn cell(&self,
-            left: char,
-            middle: char,
-            right: char,
-            fill: char,
-            content: &str,
-            pos: &Position,
-            width: usize,
-            color: &AnsiColor)
-            -> String {
+    fn cell(
+        &self,
+        left: char,
+        middle: char,
+        right: char,
+        fill: char,
+        content: &str,
+        pos: &Position,
+        width: usize,
+        color: &AnsiColor,
+    ) -> Vec<ansi_term::ANSIString<'static>> {
         let mut left_border = String::new();
         if pos.left() {
             left_border += &left.to_string();
@@ -224,9 +247,11 @@ impl<'a> CellDisplay<'a> {
         }
         let plain = AnsiColor::default();
 
-        Fixed(plain.fg).on(Fixed(plain.bg)).paint(left_border).to_string() +
-        &Fixed(color.fg).on(Fixed(color.bg)).paint(inside).to_string() +
-        &Fixed(plain.fg).on(Fixed(plain.bg)).paint(right_border).to_string()
+        vec![
+            Fixed(plain.fg).on(Fixed(plain.bg)).paint(left_border),
+            Fixed(color.fg).on(Fixed(color.bg)).paint(inside),
+            Fixed(plain.fg).on(Fixed(plain.bg)).paint(right_border),
+        ]
     }
 }
 
@@ -248,20 +273,22 @@ fn cursor_to_index(x: usize, cell_dim: usize, n_cells: usize) -> usize {
 /// display.print(&mut std::io::stdout(), &style::BordersStyle::Thin);
 /// ```
 pub struct MatrixDisplay<'a, T>
-    where T: Clone,
-          T: ToString + 'a
+where
+    T: Clone,
+    T: ToString + 'a,
 {
     fmt: &'a Format,
     mat: &'a mut Matrix<Cell<T>>,
 }
 impl<'a, T> MatrixDisplay<'a, T>
-    where T: Clone,
-          T: ToString + 'a
+where
+    T: Clone,
+    T: ToString + 'a,
 {
     /// Construct a matrix display
-	///
-	/// f: the format of a cell (width, height)
-	/// m: a reference to the data (&Matrix<Cell>)
+    ///
+    /// f: the format of a cell (width, height)
+    /// m: a reference to the data (&Matrix<Cell>)
     pub fn new(f: &'a Format, m: &'a mut Matrix<Cell<T>>) -> MatrixDisplay<'a, T> {
         MatrixDisplay { fmt: f, mat: m }
     }
@@ -271,90 +298,101 @@ impl<'a, T> MatrixDisplay<'a, T>
     fn n_cols(&self) -> usize {
         self.mat.n_cols()
     }
-	/// The matrix's width in number of characters
+    /// The matrix's width in number of characters
     pub fn width(&self) -> usize {
         self.n_cols() * self.fmt.cell_w
     }
-	/// The matrix's height in number of characters
+    /// The matrix's height in number of characters
     pub fn height(&self) -> usize {
         self.n_rows() * self.fmt.cell_h
     }
-    fn print_top<Out: Write>(&self,
-                             out: &mut Out,
-                             borders: &BordersStyle,
-                             row: &[(Cell<T>, Position)]) {
-        for &(_, ref pos) in row {
-            write!(out,
-                   "{}",
-                   CellDisplay::with_borders(borders).top_cell(pos, self.fmt.cell_w))
-                .unwrap();
-        }
+    fn print_top(
+        &self,
+        borders: &BordersStyle,
+        row: &[(Cell<T>, Position)],
+    ) -> Vec<ansi_term::ANSIString<'static>> {
+        row.iter()
+            .flat_map(|&(_, ref pos)| {
+                CellDisplay::with_borders(borders).top_cell(pos, self.fmt.cell_w)
+            })
+            .collect::<Vec<_>>()
     }
-    fn print_vertical_pad<Out: Write>(&self,
-                                      out: &mut Out,
-                                      borders: &BordersStyle,
-                                      row: &[(Cell<T>, Position)],
-                                      pad: usize) {
-        for _ in 0..pad {
-            for &(ref cell, ref pos) in row {
-                write!(out,
-                       "{}",
-                       CellDisplay::with_borders(borders)
-                           .padding_cell(pos, self.fmt.cell_w, &cell.color))
-                    .unwrap();
-            }
-        }
+    fn print_vertical_pad(
+        &self,
+        borders: &BordersStyle,
+        row: &[(Cell<T>, Position)],
+        pad: usize,
+    ) -> Vec<ansi_term::ANSIString<'static>> {
+        row.iter()
+            .cycle()
+            .take(pad)
+            .flat_map(|&(ref cell, ref pos)| {
+                CellDisplay::with_borders(borders).padding_cell(pos, self.fmt.cell_w, &cell.color)
+            })
+            .collect::<Vec<_>>()
     }
-    fn print_value_row<Out: Write>(&self,
-                                   out: &mut Out,
-                                   borders: &BordersStyle,
-                                   row: &[(Cell<T>, Position)]) {
-        for &(ref cell, ref pos) in row {
-            write!(out,
-                   "{}",
-                   CellDisplay::with_borders(borders).value_cell(pos,
-                                                                 self.fmt.cell_w,
-                                                                 &cell.clone().value.to_string(),
-                                                                 &cell.color))
-                .unwrap();
-        }
+    fn print_value_row(
+        &self,
+        borders: &BordersStyle,
+        row: &[(Cell<T>, Position)],
+    ) -> Vec<ansi_term::ANSIString<'static>> {
+        row.iter()
+            .flat_map(|&(ref cell, ref pos)| {
+                CellDisplay::with_borders(borders).value_cell(
+                    pos,
+                    self.fmt.cell_w,
+                    &cell.clone().value.to_string(),
+                    &cell.color,
+                )
+            })
+            .collect::<Vec<_>>()
     }
-    fn print_bottom<Out: Write>(&self,
-                                out: &mut Out,
-                                borders: &BordersStyle,
-                                row: &[(Cell<T>, Position)]) {
-        for &(_, ref pos) in row {
-            write!(out,
-                   "{}",
-                   CellDisplay::with_borders(borders).bottom_cell(pos, self.fmt.cell_w))
-                .unwrap();
-        }
+    fn print_bottom(
+        &self,
+        borders: &BordersStyle,
+        row: &[(Cell<T>, Position)],
+    ) -> Vec<ansi_term::ANSIString<'static>> {
+        row.iter()
+            .flat_map(|&(_, ref pos)| {
+                CellDisplay::with_borders(borders).bottom_cell(pos, self.fmt.cell_w)
+            })
+            .collect::<Vec<_>>()
     }
-	/// Print a matrix. This is the most important method of this library
+    /// Print a matrix. This is the most important method of this library
     ///
-	/// Pick a BorderStyle, an output that implements the Write trait and
-	/// you're good to go!
-	pub fn print<Out: Write>(&self, out: &mut Out, borders: &BordersStyle) {
+    /// Pick a BorderStyle, an output that implements the Write trait and
+    /// you're good to go!
+    pub fn print<Out: Write>(&self, out: &mut Out, borders: &BordersStyle) {
         let vertical_pad = Pad::new(self.fmt.cell_h, 1);
-        self.mat
+        let mat = self.mat
             .enumerate_cells()
             .chunks(self.n_cols())
             .into_iter()
             .flat_map(|row| {
-                self.print_top(out, borders, row);
-                self.print_vertical_pad(out, borders, row, vertical_pad.before);
-                self.print_value_row(out, borders, row);
-                self.print_vertical_pad(out, borders, row, vertical_pad.after);
-                self.print_bottom(out, borders, row);
-                row
+                let mut all_strings = self.print_top(borders, row);
+                all_strings.append(&mut self.print_vertical_pad(
+                    borders,
+                    row,
+                    vertical_pad.before,
+                ));
+                all_strings.append(&mut self.print_value_row(borders, row));
+                all_strings.append(&mut self.print_vertical_pad(
+                    borders,
+                    row,
+                    vertical_pad.after,
+                ));
+                all_strings.append(&mut self.print_bottom(borders, row));
+                all_strings
+                //    row
             })
             .collect::<Vec<_>>();
+        write!(out, "{}", ansi_term::ANSIStrings(&*mat)).unwrap();
     }
     /// Takes a cursor position in (usize, usize) and returns the coordinates of the cell under the cursor
     pub fn coordinates_at_cursor_position(&self, (x, y): (usize, usize)) -> (usize, usize) {
         let col = cursor_to_index(x, self.fmt.cell_w, self.mat.n_cols());
         let row = cursor_to_index(y as usize, self.fmt.cell_h, self.mat.n_rows());
-         (col, row)
+        (col, row)
     }
     /// Takes a cursor position in characters (x, y) and returns a mutable reference to the corresponding cell
     ///
@@ -362,6 +400,5 @@ impl<'a, T> MatrixDisplay<'a, T>
     pub fn cell_at_cursor_position(&mut self, cursor: (usize, usize)) -> &mut Cell<T> {
         let coord = self.coordinates_at_cursor_position(cursor);
         self.mat.at(coord)
-	}
+    }
 }
-

commit a0cd5e76a920bf22d1eb4740a997bfcdc2147a1c

Fix bug introduced in 3cc23e6
When we had to switch from a for loop to a flat_map, we got the logic
wrong for the vertical padding.

src/lib.rs
@@ -321,7 +321,7 @@ where
     ) -> Vec<ansi_term::ANSIString<'static>> {
         row.iter()
             .cycle()
-            .take(pad)
+            .take(pad * row.len())
             .flat_map(|&(ref cell, ref pos)| {
                 CellDisplay::with_borders(borders).padding_cell(pos, self.fmt.cell_w, &cell.color)
             })