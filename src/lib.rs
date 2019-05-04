71b3843f95d60acbfff0c8b91400fa2242edab57
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-05-04 14:31:50 +0100

    Replace tabs with spaces

--- src/lib.rs
+++ src/lib.rs
@@ -18,28 +18,28 @@
 //!
 //! fn main() {
 //!     let format = Format::new(7, 3);
-//! 	let board = vec!['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
-//! 	                 '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
-//! 					 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//!     let board = vec!['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
+//!                      '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟',
 //!                      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-//! 					 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-//! 					 ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-//! 					 '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖',
-//! 					 '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙']
+//!                      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//!                      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//!                      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+//!                      '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖',
+//!                      '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙']
 //!         .iter()
 //!         .enumerate()
-//! 		.map(|(i, x)| {
+//!         .map(|(i, x)| {
 //!             let ansi_fg = 33;
-//! 			let mut ansi_bg = 0;
-//! 		    if i % 2 + (i / 8) % 2 == 1 {
-//! 			    ansi_bg = 7;
-//! 			}
-//! 		    cell::Cell::new(x.clone(), ansi_fg, ansi_bg)
-//! 			})
+//!             let mut ansi_bg = 0;
+//!             if i % 2 + (i / 8) % 2 == 1 {
+//!                 ansi_bg = 7;
+//!             }
+//!             cell::Cell::new(x.clone(), ansi_fg, ansi_bg)
+//!             })
 //!         .collect::<Vec<_>>();
 //!     let mut data = matrix::Matrix::new(8, board);
 //!     let mut display = MatrixDisplay::new(&format, &mut data);
-//! 	display.cell_at_cursor_position((13, 6)).color.bg = 10;
+//!     display.cell_at_cursor_position((13, 6)).color.bg = 10;
 //!     display.print(&mut std::io::stdout(), &style::BordersStyle::None);
 //! }
 //! ```
