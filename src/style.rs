5dfa10f320eda39ad3292be5815706f8af084295
Pierre Chevalier <pierrechevalier83@gmail.com>
2019-05-12 18:56:32 +0100

    Use unicode_types for named characters

--- src/style.rs
+++ src/style.rs
@@ -4,176 +4,174 @@
 //! borders so you don't have to hunt through unicode tables
 //! and copy paste unicode symbols like an animal!
 
-pub use self::style::BordersStyle;
+use unicode_types::box_drawing::BoxDrawing;
 
-mod style {
-    /// An enum that represents different unicode borders styles
+/// An enum that represents different unicode borders styles
+///
+/// It provides accessors for specific locations such as top_left_corner to be used by the printing code
+#[derive(PartialEq)]
+pub enum BordersStyle {
+    /// ✓✓
+    /// ✓✓
+    None,
     ///
-    /// It provides accessors for specific locations such as top_left_corner to be used by the printing code
-    #[derive(PartialEq)]
-    pub enum BordersStyle {
-        /// ✓✓
-        /// ✓✓
-        None,
-        ///
-        ///  ✓ ✓
-        ///
-        ///  ✓ ✓
-        ///
-        Plain,
-        /// +-+-+
-        /// |✓|✓|
-        /// +-+-+
-        /// |✓|✓|
-        /// +-+-+
-        Retro,
-        /// ┌─┬─┐
-        /// │✓│✓│
-        /// ├─┼─┤
-        /// │✓│✓│
-        /// └─┴─┘
-        Thin,
-        /// ╭─┬─╮
-        /// │✓│✓│
-        /// ├─┼─┤
-        /// │✓│✓│
-        /// ╰─┴─╯
-        Rounded,
-        /// ┏━┳━┓
-        /// ┃✓┃✓┃
-        /// ┣━╋━┫
-        /// ┃✓┃✓┃
-        /// ┗━┻━┛
-        Thick,
-        /// ╔═╦═╗
-        /// ║✓║✓║
-        /// ╠═╬═╣
-        /// ║✓║✓║
-        /// ╚═╩═╝
-        Double,
-    }
+    ///  ✓ ✓
+    ///
+    ///  ✓ ✓
+    ///
+    Plain,
+    /// +-+-+
+    /// |✓|✓|
+    /// +-+-+
+    /// |✓|✓|
+    /// +-+-+
+    Retro,
+    /// ┌─┬─┐
+    /// │✓│✓│
+    /// ├─┼─┤
+    /// │✓│✓│
+    /// └─┴─┘
+    Thin,
+    /// ╭─┬─╮
+    /// │✓│✓│
+    /// ├─┼─┤
+    /// │✓│✓│
+    /// ╰─┴─╯
+    Rounded,
+    /// ┏━┳━┓
+    /// ┃✓┃✓┃
+    /// ┣━╋━┫
+    /// ┃✓┃✓┃
+    /// ┗━┻━┛
+    Thick,
+    /// ╔═╦═╗
+    /// ║✓║✓║
+    /// ╠═╬═╣
+    /// ║✓║✓║
+    /// ╚═╩═╝
+    Double,
+}
 
-    impl BordersStyle {
-        pub fn top_left_corner(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┌',
-                BordersStyle::Rounded => '╭',
-                BordersStyle::Thick => '┏',
-                BordersStyle::Double => '╔',
-            }
+impl BordersStyle {
+    pub fn top_left_corner(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightDownAndRight.into(),
+            BordersStyle::Rounded => BoxDrawing::LightArcDownAndRight.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyDownAndRight.into(),
+            BordersStyle::Double => BoxDrawing::DoubleDownAndRight.into(),
         }
-        pub fn top_right_corner(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┐',
-                BordersStyle::Rounded => '╮',
-                BordersStyle::Thick => '┓',
-                BordersStyle::Double => '╗',
-            }
+    }
+    pub fn top_right_corner(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightDownAndLeft.into(),
+            BordersStyle::Rounded => BoxDrawing::LightArcDownAndLeft.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyDownAndLeft.into(),
+            BordersStyle::Double => BoxDrawing::DoubleDownAndLeft.into(),
         }
-        pub fn bottom_left_corner(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '└',
-                BordersStyle::Rounded => '╰',
-                BordersStyle::Thick => '┗',
-                BordersStyle::Double => '╚',
-            }
+    }
+    pub fn bottom_left_corner(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightUpAndRight.into(),
+            BordersStyle::Rounded => BoxDrawing::LightArcUpAndRight.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyUpAndRight.into(),
+            BordersStyle::Double => BoxDrawing::DoubleUpAndRight.into(),
         }
-        pub fn bottom_right_corner(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┘',
-                BordersStyle::Rounded => '╯',
-                BordersStyle::Thick => '┛',
-                BordersStyle::Double => '╝',
-            }
+    }
+    pub fn bottom_right_corner(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightUpAndLeft.into(),
+            BordersStyle::Rounded => BoxDrawing::LightArcUpAndLeft.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyUpAndLeft.into(),
+            BordersStyle::Double => BoxDrawing::DoubleUpAndLeft.into(),
         }
-        pub fn horizontal_border(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '-',
-                BordersStyle::Thin => '─',
-                BordersStyle::Rounded => '─',
-                BordersStyle::Thick => '━',
-                BordersStyle::Double => '═',
-            }
+    }
+    pub fn horizontal_border(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '-',
+            BordersStyle::Thin => BoxDrawing::LightHorizontal.into(),
+            BordersStyle::Rounded => BoxDrawing::LightHorizontal.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyHorizontal.into(),
+            BordersStyle::Double => BoxDrawing::DoubleHorizontal.into(),
         }
-        pub fn top_intersection(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┬',
-                BordersStyle::Rounded => '┬',
-                BordersStyle::Thick => '┳',
-                BordersStyle::Double => '╦',
-            }
+    }
+    pub fn top_intersection(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightDownAndHorizontal.into(),
+            BordersStyle::Rounded => BoxDrawing::LightDownAndHorizontal.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyDownAndHorizontal.into(),
+            BordersStyle::Double => BoxDrawing::DoubleDownAndHorizontal.into(),
         }
-        pub fn left_intersection(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '├',
-                BordersStyle::Rounded => '├',
-                BordersStyle::Thick => '┣',
-                BordersStyle::Double => '╠',
-            }
+    }
+    pub fn left_intersection(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightVerticalAndRight.into(),
+            BordersStyle::Rounded => BoxDrawing::LightVerticalAndRight.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyVerticalAndRight.into(),
+            BordersStyle::Double => BoxDrawing::DoubleVerticalAndRight.into(),
         }
-        pub fn right_intersection(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┤',
-                BordersStyle::Rounded => '┤',
-                BordersStyle::Thick => '┫',
-                BordersStyle::Double => '╣',
-            }
+    }
+    pub fn right_intersection(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightVerticalAndLeft.into(),
+            BordersStyle::Rounded => BoxDrawing::LightVerticalAndLeft.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyVerticalAndLeft.into(),
+            BordersStyle::Double => BoxDrawing::DoubleVerticalAndLeft.into(),
         }
-        pub fn bottom_intersection(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┴',
-                BordersStyle::Rounded => '┴',
-                BordersStyle::Thick => '┻',
-                BordersStyle::Double => '╩',
-            }
+    }
+    pub fn bottom_intersection(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightUpAndHorizontal.into(),
+            BordersStyle::Rounded => BoxDrawing::LightUpAndHorizontal.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyUpAndHorizontal.into(),
+            BordersStyle::Double => BoxDrawing::DoubleUpAndHorizontal.into(),
         }
-        pub fn intersection(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '+',
-                BordersStyle::Thin => '┼',
-                BordersStyle::Rounded => '┼',
-                BordersStyle::Thick => '╋',
-                BordersStyle::Double => '╬',
-            }
+    }
+    pub fn intersection(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '+',
+            BordersStyle::Thin => BoxDrawing::LightVerticalAndHorizontal.into(),
+            BordersStyle::Rounded => BoxDrawing::LightVerticalAndHorizontal.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyVerticalAndHorizontal.into(),
+            BordersStyle::Double => BoxDrawing::DoubleVerticalAndHorizontal.into(),
         }
-        pub fn vertical_border(&self) -> char {
-            match *self {
-                BordersStyle::None => '\0',
-                BordersStyle::Plain => ' ',
-                BordersStyle::Retro => '|',
-                BordersStyle::Thin => '│',
-                BordersStyle::Rounded => '│',
-                BordersStyle::Thick => '┃',
-                BordersStyle::Double => '║',
-            }
+    }
+    pub fn vertical_border(&self) -> char {
+        match *self {
+            BordersStyle::None => '\0',
+            BordersStyle::Plain => ' ',
+            BordersStyle::Retro => '|',
+            BordersStyle::Thin => BoxDrawing::LightVertical.into(),
+            BordersStyle::Rounded => BoxDrawing::LightVertical.into(),
+            BordersStyle::Thick => BoxDrawing::HeavyVertical.into(),
+            BordersStyle::Double => BoxDrawing::DoubleVertical.into(),
         }
     }
 }
